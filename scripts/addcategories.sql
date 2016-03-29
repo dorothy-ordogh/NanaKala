668f97b9833e277ce232524567d333e105635e1c
Dorothy Ordogh <dorothy-ordogh@hotmail.com>
2016-03-29 12:43:23 -0700

    add script that shows and example of adding categories

---
+++ scripts/addcategories.sql
@@ -0,0 +1,10 @@
+# add rows to categories table
+
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES (0, "Gas");
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES (0, "Rent");
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES (0, "Flight");
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES (0, "Hotel");
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES (0, "Restuarants");
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES (0, "Groceries");
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES (0, "Cable");
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES (0, "Internet");
